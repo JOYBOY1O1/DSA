fc2464269f38ef2f8de9be5ff49fb8e98d3570a3
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-09-07 23:20:27 +0530

    Update pattern1.cc

--- Patterns/pattern1.cc
+++ Patterns/pattern1.cc
@@ -12,9 +12,9 @@ using namespace std;
 void pattern(int n)
 {
     int i, j;
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 5; j++)
+        for (j = 0; j < n; j++)
         {
             cout << "*";
         }
