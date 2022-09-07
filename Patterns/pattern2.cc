253eccc8b8efc3881a0abf5548382b3b373266b9
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-09-07 22:53:04 +0530

    Create pattern2.cc

---
+++ Patterns/pattern2.cc
@@ -0,0 +1,33 @@
+/*
+Create pattern:
+*
+**
+***
+****
+*****
+*/
+
+#include <iostream>
+using namespace std;
+
+void pattern(int n)
+{
+    int i, j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pattern(n);
+
+    return 0;
+}
