9a4f9098021065850c8f43b4870ef1a916ce835d
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-09-08 01:06:55 +0530

    Create pattern10.cc

---
+++ Patterns/pattern10.cc
@@ -0,0 +1,45 @@
+/*
+Create pattern:
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
+
+#include <iostream>
+using namespace std;
+
+void pattern(int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = i; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pattern(n);
+
+    return 0;
+}
