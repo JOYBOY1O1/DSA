60647f7a29e5d8902b32bf935823fe39f7f5b112
Joy khulbe <khulbejoy.jk@gmail.com>
2023-07-28 00:23:36 +0530

    Update re4.cpp

--- Recursion/re4.cpp
+++ Recursion/re4.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 void p(int i,int n){
     if(i<1)return;
-    cout<<i<<endl;
+    cout<<i;
     p(i-1,n);
+}
+
+void f(int i, int n){
+    if(i>n)return;
+    f(i+1,n); //using backtracking
+    cout<<i;
 }
 
 int main(){
 int n;
 cin>>n;
 p(n,n);
+cout<<endl;
+f(1,n);
+
 return 0;
 }
